be8fff78a3a1c530756295913192fc77c87d5192
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-09-20 16:21:35 -0500

    Import styling and style header buttons, add contact and menu files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import { loadPage } from "./page";
 import FoodPic from './pexels-sydney-troxell-718742.jpg';
+import { loadPage } from "./page";
+import './style.css';
 
 (function () {
-    const content = document.querySelector('#content');
+    document.body.classList.add('general');
+
+    const content = document.createElement('div');
+    content.setAttribute('id', 'content');
 
     const headline = document.createElement('h1');
     headline.textContent = "Gabriel's Bare Bone's Restaurant!";
@@ -14,4 +18,22 @@ import FoodPic from './pexels-sydney-troxell-718742.jpg';
     description.textContent = 'Best restaurant in the world, as rated by all critics!';
 
     loadPage(content, headline, myImage, description);
+    document.body.appendChild(content);
+
+    const header = document.getElementById('header');
+
+    const homeBtn = document.createElement('button');
+    homeBtn.classList.add('button');
+    homeBtn.textContent = 'Home';
+    header.appendChild(homeBtn);
+
+    const menuBtn = document.createElement('button');
+    menuBtn.classList.add('button');
+    menuBtn.textContent = 'Menu';
+    header.appendChild(menuBtn);
+    
+    const contactBtn = document.createElement('button');
+    contactBtn.classList.add('button');
+    contactBtn.textContent = 'Contact';
+    header.appendChild(contactBtn);
 })();
--- src/page.js
+++ src/page.js
@@ -1,6 +1,22 @@
+import './style.css';
+
 function loadPage(content, headline, image, description) {
+    content.classList.add('general');
+
+    const header = document.createElement('div');
+    header.classList.add('general');
+    header.classList.add('header');
+    header.setAttribute('id', 'header');
+    content.appendChild(header);
+
+    headline.classList.add('general');
     content.appendChild(headline);
+
+    image.style.width = '300px';
+    image.style.height = 'auto';
     content.appendChild(image);
+
+    description.classList.add('general');
     content.appendChild(description);
 }
 
